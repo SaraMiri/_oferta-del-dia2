f1815eb79c0b8dd4666d126e49bdd891c89a1f06
Sara Miragaya Rico <saramiragaya@gmail.com>
2018-11-14 09:34:59 +0100

    Add information on README and StorePicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { getFunName } from "../helpers";
+import { navigate } from "@reach/router";
 
 class StorePicker extends React.Component {
+  inputStore = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the <form> from submitting
+    event.preventDefault();
+    // 2. Get the text from <input>
+    const myInput = this.inputStore.current;
+    const storeName = myInput.value;
+    // 3. Change the page to /sotre/name-of-the-store
+    navigate(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          ref={this.inputStore}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">
+          {" "}
           Visit Store {""}
           <span role="img" aria-labelledby="sushi-box">
             🍱
